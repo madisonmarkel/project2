fbb3d2b5e7455021df51e240732b2f2a03e4025a
Greg Turner <jgregoryturner@gmail.com>
2018-10-30 20:52:42 -0600

    added passport code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,72 @@
 var db = require("../models");
+var passport = require("../config/passport");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Recipe.findAll({}).then(function(data) {
-      res.json(data);
+
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+
+    res.json("/members");
+  });
+
+  app.post("/api/signup", function(req, res) {
+    console.log(req.body);
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password
+    }).then(function() {
+      res.redirect(307, "/api/login");
+    }).catch(function(err) {
+      console.log(err);
+      res.json(err);
+      // res.status(422).json(err.errors[0].message);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Recipe.create(req.body).then(function(data) {
-      res.json(data);
-    });
+  // Route for logging user out
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Recipe.destroy({ where: { id: req.params.id } }).then(function(data) {
-      res.json(data);
-    });
+  // Route for getting some data about our user to be used client side
+  app.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    }
+    else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        email: req.user.email,
+        id: req.user.id
+      });
+    }
   });
+
+    // Get all examples
+    app.get("/api/examples", function (req, res) {
+      db.Recipe.findAll({}).then(function (data) {
+        res.json(data);
+      });
+    });
+  
+    // Create a new example
+    app.post("/api/examples", function (req, res) {
+      db.Recipe.create(req.body).then(function (data) {
+        res.json(data);
+      });
+    });
+  
+    // Delete an example by id
+    app.delete("/api/examples/:id", function (req, res) {
+      db.Recipe.destroy({
+        where: {
+          id: req.params.id
+        }
+      }).then(function (data) {
+        res.json(data);
+      });
+    });
+
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,42 @@
 var db = require("../models");
 
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 module.exports = function(app) {
+  
+  // Login Passport routes that we need to figure out how it works with our handlebars
+  //
+  //-------------------------------------------------------------
+
+  app.get("/", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+  //-----------------------------------------------
+  //
+
+
   // Load index page
   app.get("/", function(req, res) {
     db.Recipe.findAll({}).then(function(dbExamples) {
