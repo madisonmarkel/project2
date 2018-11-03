2fb5c139606c331a90be6ef6693c29c70b3fd3ab
Alyssa W <awilliams62090@gmail.com>
2018-11-03 11:40:38 -0600

    user creation working and password hashing is good.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -26,7 +26,7 @@ module.exports = function(app) {
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
     
-    res.render("index");
+    res.render("members");
   });
   //-----------------------------------------------
   //
